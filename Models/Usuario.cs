2d8bd3e3777ea5b1af6da628df0bda6c5bf9f780
FIC <FIC@info.local>
2021-02-12 17:55:12 -0200

    Controller e Model foram criadas

---
+++ Models/Usuario.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using InstaDev.Interfaces;
+
+namespace InstaDev.Models
+{
+    public class Usuario : InstaDevBase, IUsuario
+    {
+        public int IdUsuario { get; set; }
+        
+        public string Nome { get; set; }
+        
+        public string Foto { get; set; }
+        
+        public string DataDeNascimento { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+        
+        public string Senha { get; set; }
+
+
+        public const string PATH ="Database/Usuario.csv"
+
+        public string PrepararLinha(Usuario u)
+        {
+            return $"{u.IdUsuario};{u.Nome};{u.Foto};{u.DataDeNascimento};{u.Email};{u.Usarname};{u.Senha}"
+        }
+
+
+        public void Cadastrar(Usuario user)
+        {
+            string[] linha = { PrepararLinha(user) };
+            File.AppendAllLines(PATH, linha);
+        }
+    }
+}
